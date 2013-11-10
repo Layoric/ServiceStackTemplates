499df096b398fd10c3e9b1b2be0609b0eb3816a8
Darren Reid <layoric@gmail.com>
2013-11-10 11:04:40 +1100

    Added VS projects/slns that project templates are generated off

---
+++ Original VS Projects/ServiceStack Templates/EmptyASPNETServiceStack/ApplicationHost.cs
@@ -0,0 +1,20 @@
+﻿using Funq;
+using ServiceStack.WebHost.Endpoints;
+
+namespace EmptyServiceStack
+{
+    public class ApplicationHost : AppHostBase
+    {
+        public ApplicationHost() //Tell ServiceStack the name and where to find your web services
+            : base("Empty ASP.NET ServiceStack Project", typeof(ApplicationHost).Assembly)
+        {
+            //Configure User Defined REST Paths via 'Routes' or Attribute request DTOs
+            
+        }
+
+        public override void Configure(Container container)
+        {
+            //Configure IoC
+        }
+    }
+}
